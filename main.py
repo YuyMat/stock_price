b38600110e653c946f82c9b50ad5199bc7deea5d
Yuya, Matsumoto <yuya2005m@gmail.com>
2024-07-15 04:04:56 +0900

    fix InitialPrice class

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import random
 
 # 初期値の設定
-# mode=rはランダムor任意の数字で指定
 class InitialPrice(object):
-    def __init__(self, mode):
-        self.initial_price = 0
-
-        # モード選択
-        if mode == "r":
-            initial_price_list = [100, 300, 500, 700, 900, 1100]
-            self.choice_number = random.choice(initial_price_list)
-            self.initial_price = self.decide_the_number()
-        elif type(mode) == int:
-            self.choice_number = mode
-            self.initial_price = self.decide_the_number()
+    # モード選択、mode=rはランダムor任意の数字で指定
+    def select_mode(self):
+        try:
+            self.mode = int(input("Select the mode, r or initial pricer: "))
+        except:
+            self.mode = "r"
     
-        print(self.initial_price)
-    
-    # 完全決定
-    def decide_the_number(self):
-        times_number = random.uniform(0.9, 1.1)
-        return round(self.choice_number * times_number, 2)
+    # モードによって初期値分け
+    def initial_price(self):
+        if self.mode == "r":
+            return random.randint(90, 200)
+        if type(self.mode) == int:
+            return self.mode
     
 class FirstUpDown():    
-    def up_down_value(self, mode):
-        self.initial_price = InitialPrice(mode).initial_price
+    def up_down_value(self):
+        self.initial_price = InitialPrice().initial_price
 
+        print(self.initial_price)
         plus_minus_value = random.uniform(self.initial_price * 0.01, self.initial_price * 0.02)
         random_plus_minus = random.randint(0, 1)
 
@@ -42,6 +37,6 @@ class FirstUpDown():
 
 
 if __name__ == "__main__":
-    mode = input("Choose the mode, r(Random) or value：")
-    first = FirstUpDown()
-    first.up_down_value(mode)
+    a = InitialPrice()
+    a.select_mode()
+    print(a.initial_price())
